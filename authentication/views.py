71ffae0a8a4ede7a966f955df47bee7970d6d4ea
ByteNirush <nirushstha@gmail.com>
2025-10-12 21:09:15 +0545

    feat(views): Add register_view to handle user registration with RegisterForm

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import views as auth_views
+from .forms import RegisterForm
 
-# Create your views here.
+def register_view(request):
+    #Register new user
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+        
+    else:
+        form = RegisterForm()
+    return render(request, 'register.html', {'form': form})
+            
