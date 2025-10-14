5b6b434df7266968762e68de7f2ff16f0a387257
ByteNirush <nirushstha@gmail.com>
2025-10-14 09:17:20 +0545

    create AuthenticationForm

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,10 +1,34 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
 class RegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={
+        'placeholder': 'you@example.com'
+    }))
     
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+        widgets = {
+            'username': forms.TextInput(attrs={'placeholder': 'Pick a unique username', 'autofocus': 'autofocus'}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password1'].widget.attrs.update({'placeholder': '8+ characters with letters and numbers'})
+        self.fields['password2'].widget.attrs.update({'placeholder': 'Re-enter your password'})
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Enter your username',
+            'autofocus': 'autofocus'
+        })
+    )
+    password = forms.CharField(
+        strip=False,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Enter your password'
+        })
+    )
